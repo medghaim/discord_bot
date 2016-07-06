2605dbdab51e332356e2b1579a03c20e948954bf
Mohammad Edghaim <m.edghaim@gmail.com>
2016-07-06 02:01:46 -0400

    Added math expression parsing with !math

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -1,14 +1,16 @@
 from discord.ext import commands
 from cogs.utils import output
+from cogs.utils import math
 
 class Utilities():
 	def __init__(self, bot):
 		self.bot = bot
 
 	@commands.command()
-	async def math(self, *args : str):
+	async def math(self, expr : str):
 		""" Evaluates a math expression """
-		await output.speak(self.bot, ", ".join(args))
+		await output.speak(self.bot, math.eval_expr(expr))
+		#await output.speak(self.bot, ", ".join(args))
 		
 
 def setup(bot):
