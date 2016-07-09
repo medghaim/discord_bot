9fb3dd38a7108e751237dde745a89554aef10b0e
Mohammad Edghaim <m.edghaim@gmail.com>
2016-07-09 02:07:22 -0400

    formatting cleanup

--- utils/admin_utils.py
+++ utils/admin_utils.py
@@ -3,23 +3,23 @@ import discord
 from discord.ext import commands
 
 async def ban_dispenser(bot, ctx, time, members):
-	if time == 0:
+	if time == 0:	# INDEFINITE BAN
 		for member in members:
 			await bot.send_message(member, 'You have been indefinitely banned from the server. _Jah praise_.')
 			await bot.ban(member)
 		await bot.say('Indefinitely Banned: \n\t{}'.format(", ".join(m.name for m in members)))
-	else:
+
+	else:			# TEMP BAN
 		# send messages to all the members specified and ban then
-		banstr = 'Banned for {} mins:\n'.format(time)
+		banstr = ''
 		for member in members:
 			inv = (await bot.create_invite(ctx.message.server)).url
 			banstr += '\t{}\n'.format(member.name)
 			await bot.send_message(member, 'You have been banned for {} mins.\nYou will be able to use the link below to rejoin the server when your ban is automatically lifted.\n{}'.format(time, inv))
 			await bot.ban(member)
-		await bot.say(banstr)
 
-		# wait for `time` minutes
-		await asyncio.sleep(time*60)
+		await bot.say('Banned for {} mins:\n{}'.format(time, banstr))
+		await asyncio.sleep(time*60) # wait for `time` minutes
 		
 		# unban all the members
 		for member in members:	
